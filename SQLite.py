018b940ee08c52053d0b3e0fea515ada6572e3f2
saaulgs01 <91549407+saaulgs01@users.noreply.github.com>
2023-03-26 16:41:35 +0200

    Add files via upload

--- SQLite.py
+++ SQLite.py
@@ -2,203 +2,66 @@ import sqlite3
 from sqlalchemy import create_engine
 
 import pandas as pd
-conn = sqlite3.connect('ejemplo1.db')
-
-filename = 'alerts.csv'
-data = pd.read_csv(filename, header=0)
-print(conn)
-#print(data.shape)
-#print(data.head(10))
-
-pd.options.display.max_columns= 50
-pd.options.display.max_rows= 50
-
-filename2 = 'devices.json'
-data2 = pd.read_json(filename2)
-#print(data2.shape)
-print(data2)
-#print(data2.values)
 
 
-df1 = data2.reindex(columns=['id','ip','localizacion'])
-print(df1)
-print("\n")
-
-#df2 = data2.reindex(columns=['responsable'])
-columnas = ['nombre', 'telefono', 'rol']
-df2 = pd.DataFrame(data2['responsable'].tolist()).reindex(columns=columnas)
-print(df2)
-print("\n")
-
-#df3 = data2.reindex(columns=['analisis'])
-columnas2 = ['puertos_abiertos, servicios', 'servicios_inseguros','vulnerabilidades_detectadas']
-df3 = pd.DataFrame(data2['analisis'].tolist()).reindex(columns=columnas2)
-print(df3)
-print("\n")
-
-
-#df3['puertos_abiertos'] = df3['puertos_abiertos'].astype('string')
-#data.to_sql(name='probin',con=conn, if_exists='append')
-#df1.to_sql(name='devices',con=conn, if_exists='append')
-#df2.to_sql(name='responsable',con=conn, if_exists='append')
-#df3.to_sql(name='analisis',con=conn, if_exists='append')
-#df4.to_sql(name='puertos_abiertos',con=conn, if_exists='append')
-
-
-
-
-
-def sql_update(con):
-    cursorObj = con.cursor()
-    cursorObj.execute('UPDATE analisis SET puertos_abiertos = "None" where puertos_abiertos = "NULL"')
+def practica1():
+    con = sqlite3.connect('Practica1.db')
+    cur = con.cursor()
+    cur.execute('drop table if exists alertas')
+    cur.execute('drop table if exists analisis')
+    cur.execute('drop table if exists devices')
+    cur.execute('drop table if exists puertos_abiertos')
+    cur.execute('drop table if exists responsable')
+    con.commit()
+    cur.execute("CREATE TABLE if NOT exists alertas ('index' integer, timestamp datetime,sid INTEGER, msg TEXT,clasificacion TEXT,prioridad INTEGER,protocolo TEXT,origen TEXT,destino TEXT,puerto TEXT, PRIMARY KEY('index'),FOREIGN KEY(destino) REFERENCES devices(ip),FOREIGN KEY(origen) REFERENCES devices(ip))")
+    cur.execute("CREATE TABLE IF NOT EXISTS analisis ('index' integer, servicios INTEGER, servicios_inseguros INTEGER, vulnerabilidades_detectadas INTEGER ,PRIMARY KEY('index'),FOREIGN KEY('index') REFERENCES devices('index'))")
+    cur.execute("CREATE TABLE IF NOT EXISTS devices ('index' integer, id TEXT, ip TEXT, localizacion TEXT, PRIMARY KEY('index'))")
+    cur.execute("CREATE TABLE IF NOT EXISTS puertos_abiertos ('index' integer, puerto TEXT, device integer, FOREIGN KEY(device) REFERENCES analisis('index'))")
+    cur.execute("CREATE TABLE IF NOT EXISTS responsable ('index' integer, nombre TEXT, telefono TEXT, ROL TEXT, FOREIGN KEY(nombre) REFERENCES devices('index'))")
     con.commit()
 
-def sql_fetch(con):
-   cursorObj = con.cursor()
-   cursorObj.execute('SELECT * FROM devices')
-   #SELECT dni, nombre FROM usuarios WHERE altura > 1.0
-   rows = cursorObj.fetchall()
-   for row in rows:
-      print(row)
 
-def sql_delete(con):
-    cursorObj = con.cursor()
-    cursorObj.execute('DELETE FROM analisis where vulnerabilidades_detectadas = "15"')
+    filename = 'alerts.csv'
+    data = pd.read_csv(filename, header=0)
+
+    pd.options.display.max_columns= 50
+    pd.options.display.max_rows= 50
+
+    filename2 = 'devices.json'
+    data2 = pd.read_json(filename2)
+
+    print(data2)
+    df1 = data2.reindex(columns=['id','ip','localizacion'])
+
+    df2 = data2.reindex(columns=['responsable'])
+    columnas = ['nombre', 'telefono', 'rol']
+    df2 = pd.DataFrame(data2['responsable'].tolist()).reindex(columns=columnas)
+    print(data2)
+
+
+    df3 = data2.reindex(columns=['analisis'])
+    columnas2 = ['servicios', 'servicios_inseguros','vulnerabilidades_detectadas']
+    df3 = pd.DataFrame(data2['analisis'].tolist()).reindex(columns=columnas2)
+
+    data.to_sql(name='alertas', con=con,if_exists='append')
+    df1.to_sql(name='devices',con=con, if_exists='append')
+    df2.to_sql(name='responsable',con=con, if_exists='append')
+    df3.to_sql(name='analisis',con=con, if_exists='append')
+    cur.execute("INSERT into puertos_abiertos ('index', 'puerto','device') values (1,'80/TCP',0),(2,'443/TCP',0),(3,'3306/TCP',0),(4,'40000/UDP',0),(5,'None',1),(6,'1194/UDP',2),(7,'8080/TCP',2),(8,'8080/UDP',2),(9,'40000/UDP',2),(10,'443/UDP',3),(11,'80/TCP',3),(12,'80/TCP',4),(13,'67/UDP',4),(14,'68/UDP',4),(15,'8080/TCP',5),(16,'3306/TCP',5),(17,'3306/UDP',5),(18,'80/TCP',6),(19,'443/TCP',6),(20,'9200/TCP',6),(21,'9300/TCP',6),(22,'5601/TCP',6)")
+    cur.execute("update devices set localizacion = NULL where localizacion = 'None'")
+    cur.execute("update puertos_abiertos set puerto = NULL where puerto = 'None'")
     con.commit()
 
-def sql_delete_table(con):
-    cursorObj = con.cursor()
-    cursorObj.execute('drop table if exists usuarios1')
-    con.commit()
 
-def sql_create_table(con):
-    cursorObj = con.cursor()
-    cursorObj.execute("CREATE TABLE IF NOT EXISTS devices (id text, ip text, localization text)")
-    cursorObj.execute("CREATE TABLE IF NOT EXISTS responsable (nombre text, telefono text, rol text)")
-    cursorObj.execute("CREATE TABLE IF NOT EXISTS analisis (puertos_abiertos text, servicios integer, servicios_inseguros integer, vulnerabilidades_detectadas integer)")
-  #  cursorObj.execute("INSERT INTO devices VALUES ('web', '172.18.0.0', 'None') ")
-   # cursorObj.execute("INSERT INTO responsable VALUES ('admin', '656445552', 'Administrador de sistemas') ")
-    cursorObj.execute("INSERT INTO analisis VALUES ('80/TCP, 443/TCP, 3306/TCP, 40000/UDP,', '3', '0', '15') ")
+    #EJERCICIO2 (2 PUNTOS)
+    #cuestion 1:
+    preg1 = "SELECT COUNT(*) as devices FROM devices"
+    result = pd.read_sql_query(preg1, con)
+    print(result)
 
 
-  #  con.commit()
-
-#con = sqlite3.connect('ejemplo1.db')
-#sql_create_table(con)
-#sql_fetch(con)
-#sql_update(con)
-#sql_fetch(con)
-#sql_delete(con)
-#sql_fetch(con)
-#sql_delete_table(con)
-
-#EJERCICIO2 (2 PUNTOS)
-
-#cuestion 1:
-preg1 = "SELECT COUNT(*) as devices FROM devices"
-result = pd.read_sql_query(preg1, conn)
-print(result)
-
-preg2 = "SELECT COUNT(*) as campos_None FROM devices where localizacion is 'None'"
-result = pd.read_sql_query(preg2, conn)
-print(result)
-
-#Cuestrion 2:
-preg = "select count(*) as alertas from alertas"
-result = pd.read_sql_query(preg, conn)
-print(result)
-#Cuestion 3:
-
-preg = "select count(*) as Media_puertos from puertos_abiertos"
-preg = "SELECT AVG(device) AS media_puertos_abiertos FROM puertos_abiertos INNER JOIN analisis ON puertos_abiertos.device = analisis.indice"
-result = pd.read_sql_query(preg, conn)
-print(result)
-#cuestion 4:
-
-preg = "select avg(servicios_inseguros) as mediaServicios_inseguros from analisis"
-result = pd.read_sql_query(preg, conn)
-print(result)
-
-preg = "SELECT AVG((puerto - sub.a) * (puerto - sub.a)) as var from puertos_abiertos, (SELECT AVG(puerto) AS a FROM puertos_abiertos) AS sub"
-result = pd.read_sql_query(preg, conn)
-print(result)
-
-#cuestion 5:
-preg = "select avg(vulnerabilidades_detectadas) as media_vulnerabilidades_detectadas from analisis"
-result = pd.read_sql_query(preg, conn)
-print(result)
-#cuestion 6:
-
-preg = "SELECT COUNT(device) as cantidad_maxima, device FROM puertos_abiertos GROUP BY device ORDER BY cantidad_maxima DESC LIMIT 1"
-result = pd.read_sql_query(preg, conn)
-print(result)
-
-preg = "SELECT min(count_val) as cantidad_minima,device FROM (SELECT COUNT(case WHEN puerto IS 'None' then NULL else device END) as count_val, device FROM puertos_abiertos GROUP BY device order by count_val) as counts"
-result = pd.read_sql_query(preg, conn)
-print(result)
-#Cuestion 7:
-
-preg = "select min(vulnerabilidades_detectadas) as min_vul from analisis"
-result = pd.read_sql_query(preg, conn)
-print(result)
-
-preg = "select max(vulnerabilidades_detectadas) as max_vul from analisis"
-result = pd.read_sql_query(preg, conn)
-print(result)
-
-#EJERCICIO3 (2.5 PUNTOS)
-#cuestion 1:
-preg = "select count(vulnerabilidades_detectadas) as vulnerabilidades_detectadas, strftime('%m', fechas) as mes, prioridad  from analisis inner join devices on analisis.indice = devices.indice inner join alertas on devices.ip=alertas.origen or devices.ip=alertas.destino where  strftime('%m', fechas) IN ('07','08') group by prioridad,mes order by mes"
-result = pd.read_sql_query(preg, conn)
-print(result)
-#cuestion 2:
-preg = "select count(vulnerabilidades_detectadas) as campos_none, strftime('%m', fechas) as mes, prioridad  from analisis inner join devices on analisis.indice = devices.indice inner join alertas on devices.ip=alertas.origen or devices.ip=alertas.destino where  localizacion='None' and strftime('%m', fechas) IN ('07','08') group by prioridad,mes order by mes"
-result = pd.read_sql_query(preg, conn)
-print(result)
-
-#cuestion 3:
-preg = "select median(vulnerabilidades_detectadas) as mediana_vulnerabilidades_detectadas, strftime('%m', fechas) as mes, prioridad  from analisis inner join devices on analisis.indice = devices.indice inner join alertas on devices.ip=alertas.origen or devices.ip=alertas.destino where  strftime('%m', fechas) IN ('07','08') group by prioridad,mes order by mes"
-#result = pd.read_sql_query(preg, conn)
-print(result)
-#cuestion 4:
-preg = "select avg(vulnerabilidades_detectadas) as media_vulnerabilidades_detectadas, strftime('%m', fechas) as mes, prioridad  from analisis inner join devices on analisis.indice = devices.indice inner join alertas on devices.ip=alertas.origen or devices.ip=alertas.destino where  strftime('%m', fechas) IN ('07','08') group by prioridad,mes order by mes"
-result = pd.read_sql_query(preg, conn)
-print(result)
-#cuestion 5:
-preg = "select stdev(vulnerabilidades_detectadas) as varvulnerabilidades_detectadas, strftime('%m', fechas) as mes, prioridad  from analisis inner join devices on analisis.indice = devices.indice inner join alertas on devices.ip=alertas.origen or devices.ip=alertas.destino where  strftime('%m', fechas) IN ('07','08') group by prioridad,mes order by mes"
-#result = pd.read_sql_query(preg, conn)
-print(result)
-#cuestion 6:
-preg = "select max(vulnerabilidades_detectadas) as max_vulnerabilidades_detectadas, strftime('%m', fechas) as mes, prioridad  from analisis inner join devices on analisis.indice = devices.indice inner join alertas on devices.ip=alertas.origen or devices.ip=alertas.destino where  strftime('%m', fechas) IN ('07','08') group by prioridad,mes order by mes"
-result = pd.read_sql_query(preg, conn)
-print(result)
-#cuestion 6:
-preg = "select min(vulnerabilidades_detectadas) as min_vulnerabilidades_detectadas, strftime('%m', fechas) as mes, prioridad  from analisis inner join devices on analisis.indice = devices.indice inner join alertas on devices.ip=alertas.origen or devices.ip=alertas.destino where  strftime('%m', fechas) IN ('07','08') group by prioridad,mes order by mes"
-result = pd.read_sql_query(preg, conn)
-print(result)
+    con.close()
 
 
+practica1()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-conn.close()
